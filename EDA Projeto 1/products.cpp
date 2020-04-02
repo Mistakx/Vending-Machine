42b8d07b39ac94c163c3a5470d453ccd960eeb1d
Mistakx <ade9@live.com.pt>
2020-04-02 03:46:53 +0100

    Initialization Process Complete

--- EDA Projeto 1/products.cpp
+++ EDA Projeto 1/products.cpp
@@ -3,19 +3,61 @@
 using namespace std;
 
 
+void print_array(string* array, int lenght) {
 
-void place_product() { // Places random product on the chosen slot, and assigns that slot a price.
+	for (int i = 0; i < lenght; i++) {
 
-	rand() %  // Generates a random number between 0 and the number of products still available (including both)
+		cout << array << "[" << i << "]: " << array[i] << endl;
 
+	}
+
+} // DEBUG
+
+products* delete_product(products* struct_to_delete_from, int* index_to_delete) { // Returns the same struct with a chosen array element removed.
+
+	products new_struct;
+	// The new struct array size is one less because one element was removed.
+	new_struct.array = new string[(struct_to_delete_from->lenght) - 1]; 
+	new_struct.lenght = (struct_to_delete_from->lenght) - 1;
+
+	
+	// Adds the elements that are located before the element that's going to be deleted, to the new array.
+	for (int i = 0; i < *index_to_delete; i++) {
+
+		new_struct.array[i] = struct_to_delete_from->array[i];
+
+	}
+
+
+	// Adds the elements that are located after the element that's going to be deleted, to the new array.
+	for (int i = (*index_to_delete + 1); i < struct_to_delete_from->lenght; i++) {
+
+		new_struct.array[i - 1] = struct_to_delete_from->array[i];
+
+	}
+
+
+	new_struct.lenght = struct_to_delete_from->lenght - 1;
+
+
+	return &new_struct;
+
+}
+
+string choose_random_product(products* products_to_choose_from) { // Chooses random product from a struct, then removes that product from the struct.
+	
+
+	int random_product_index = rand() % products_to_choose_from->lenght; // Generates a random number between 0 and the number of products still available (including both).
+	string chosen_product_name = products_to_choose_from->array[random_product_index]; // Chooses a product from the initialization_products array.
+	
+	*products_to_choose_from = *delete_product(products_to_choose_from, &random_product_index);
+
+	return chosen_product_name;	
 	
 
 }
 
-void product_initialization(product product_to_initialize) {
 
 
 
-
-}
 
--- EDA Projeto 1/vending_machine.cpp
+++ EDA Projeto 1/vending_machine.cpp
@@ -2,19 +2,24 @@
 #include "header.h"
 using namespace std;
 
-slot* vending_machine_initialization() {
+slot* vending_machine_initialization(products* initialization_products, prices* text_prices) {
+
+
 
 	int number_of_slots = rand() % 4 + 9; // Generates a random number between 9 and 12 (including both)
 
-	slot* pvending_machine = new slot[number_of_slots]; // The vending machine is an array of slots.
+	slot* vending_machine = new slot[number_of_slots]; // The vending machine is an array of slots.
 	
 	for (int i = 0; i < number_of_slots; i++) {
-		slot_initialization(pvending_machine[i], i); // Vending Machine is an array of slots. Type: slot*. Each slot is a struct so there's no need to pass by reference. 
+
+		slot_initialization(vending_machine[i], i, initialization_products, text_prices); // Vending Machine is an array of slots. Type: slot*. Each slot is a struct so there's no need to pass by reference. 
 		// Thanks debugger for the help :D. Usually print debugging does the work but not this time.
 
 	}
 
-	return pvending_machine; // Returns the adress of the first slot.
+	print_array(initialization_products->array, initialization_products->lenght);
+
+	return vending_machine; // Returns the adress of the first slot.
 
 }
 
